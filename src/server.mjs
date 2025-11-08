c28c832fe281cd65653030ea4ac1e160202128bc
konaOnTheHub <nagy.petike2@gmail.com>
2025-11-08 13:31:36 +0000

    Implemented connection to MongoDB

--- src/server.mjs
+++ src/server.mjs
@@ -1,9 +1,40 @@
 import express from 'express';
+import dotenv from 'dotenv';
+//Inject secret URI as an environment variable from .env file
+dotenv.config();
+
+import { MongoClient, ServerApiVersion } from 'mongodb';
+//Use environment variable for URI
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+let lessonsCollection;
+
+async function connectDB() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ping: 1});
+        console.log("Connected to MongoDB successfully");
+
+        const database = client.db("NextClassDB");
+        lessonsCollection = database.collection("lessons");
+    } catch (err) {
+        console.error("MongoDB connection error:", err);
+    }
+}
 
 const app = express();
 
 const PORT = process.env.PORT || 5555;
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+    await connectDB();
     console.log(`Running on Port ${PORT}`);
 })
